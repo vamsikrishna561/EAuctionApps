e5b386c3c12af752690ac4cf18d2d4396afa0536
Vinoth Ramalingam <274050@cognizant.com>
2022-07-02 17:05:51 +0530

    first commit

--- services/E-Auction.API/EAuction.API/Startup.cs
+++ services/E-Auction.API/EAuction.API/Startup.cs
@@ -1,7 +1,9 @@
+using E_Auction.Infrastructure.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
